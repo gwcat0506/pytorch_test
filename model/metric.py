37730179592163c252adb964c372edf260dc25c9
gwcat0506 <61350052+gwcat0506@users.noreply.github.com>
2023-12-12 11:47:59 +0900

    MAE, MSE, RMSE 추가

--- model/metric.py
+++ model/metric.py
@@ -1,20 +1,16 @@
-import torch
+# import torch
+import sklearn.metrics as metrics
+import numpy as np
+
+# MAE, MSE, RMSE 추가
+def MAE(output, target):
+    return metrics.mean_absolute_error(target, output)
+
+def MSE(output, target):
+    return metrics.mean_squared_error(target, output)
+
+def RMSE(output, target):
+    return np.sqrt(metrics.mean_squared_error(target, output))
 
 
-def accuracy(output, target):
-    with torch.no_grad():
-        pred = torch.argmax(output, dim=1)
-        assert pred.shape[0] == len(target)
-        correct = 0
-        correct += torch.sum(pred == target).item()
-    return correct / len(target)
 
-
-def top_k_acc(output, target, k=3):
-    with torch.no_grad():
-        pred = torch.topk(output, k, dim=1)[1]
-        assert pred.shape[0] == len(target)
-        correct = 0
-        for i in range(k):
-            correct += torch.sum(pred[:, i] == target).item()
-    return correct / len(target)
