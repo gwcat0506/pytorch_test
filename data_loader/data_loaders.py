15231d9a0f0e0eeec785daeba526c740fa4cfff9
gwcat0506 <61350052+gwcat0506@users.noreply.github.com>
2023-12-12 11:47:04 +0900

    data loader 설정

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,16 +1,43 @@
-from torchvision import datasets, transforms
-from base import BaseDataLoader
+import torch
+from torch.utils.data import Dataset, DataLoader
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.model_selection import train_test_split
+
+# Dataset Loader 
+class TimeseriesDatasetLoader(Dataset):
+    def __init__(self, series, window_size, shuffle):
+        self.series = torch.tensor(series.values).float()
+        self.window_size = window_size
+        self.shuffle = shuffle
+
+    def __len__(self):
+        return len(self.series) - self.window_size - 1
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        if self.shuffle:
+            idx = torch.randint(len(self), size=(1,)).item()
+
+        return (self.series[idx:idx+self.window_size], self.series[idx+self.window_size+1])
 
 
-class MnistDataLoader(BaseDataLoader):
-    """
-    MNIST data loading demo using BaseDataLoader
-    """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
-        trsfm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])
-        self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
-        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+def get_loader(series, window_size, batch_size, shuffle=True):
+    dataset = TimeseriesDatasetLoader(series, window_size, shuffle)
+    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+
+# data split func
+def split_data(df, split_ratio):
+    _, _, y_train, y_test = train_test_split(
+    df.drop('traffic', axis=1), 
+    df['traffic'], 
+    test_size=split_ratio, 
+    shuffle=False)
+    return y_train, y_test
+
+def apply_scaler(df):
+    scaler = MinMaxScaler()
+    scale_cols = ['traffic']
+    df['traffic'] = scaler.fit_transform(df[scale_cols])
+    return df
