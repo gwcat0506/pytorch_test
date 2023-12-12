449a2d7f0726a1c0b83a0c54bb9e67abc78430ca
gwcat0506 <61350052+gwcat0506@users.noreply.github.com>
2023-12-12 11:47:38 +0900

    mse_loss 추가

--- model/loss.py
+++ model/loss.py
@@ -1,5 +1,7 @@
 import torch.nn.functional as F
+import torch.nn as nn
 
-
-def nll_loss(output, target):
-    return F.nll_loss(output, target)
+# mse_loss 추가 
+def mse_loss(output, target):
+    mse_loss = nn.MSELoss()
+    return mse_loss(output.squeeze(1), target)
