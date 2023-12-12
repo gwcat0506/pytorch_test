0d0386812b2119fc87190703c96414142f43137f
gwcat0506 <61350052+gwcat0506@users.noreply.github.com>
2023-12-12 11:48:29 +0900

    LSTM 모델 구성

--- model/model.py
+++ model/model.py
@@ -1,22 +1,27 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
 
 
-class MnistModel(BaseModel):
-    def __init__(self, num_classes=10):
-        super().__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, num_classes)
+import torch.nn as nn
+
+# 모델 구성
+# Applying Deep Learning Approaches for Network Traffic Prediction (ICACCI, 2017) 
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(LSTM, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
+                            num_layers=num_layers, batch_first=True)
+        self.fc1 = nn.Linear(hidden_size, num_classes)
+        self.params = self.lstm.state_dict()
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).cuda()
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).cuda()
+        out, _ = self.lstm(x.unsqueeze(2), (h0, c0))
+        last_out = out[:, -1, :]
+        out = self.fc1(last_out)
+        return out
